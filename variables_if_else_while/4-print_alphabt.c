b610ddab04a0fb9ed85e6c9b3f3620d97527ca6f
10ml10 <lisacarino10@gmail.com>
2025-06-17 09:02:32 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - affiche l'alphabet minuscules sans q et e, suivi d'un retour à la ligne
+ *
+ * Return: toujours 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
