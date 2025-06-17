6bcc4547f24ae5d88cae050b5ff2149c335de85d
10ml10 <lisacarino10@gmail.com>
2025-06-17 12:48:11 +0000

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche les chiffres de base 16 en minuscules, suivis d'un retour à la ligne
+ *
+ * Return: Toujours 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+
+	i = 'a';
+
+	while (i <= 'f')
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
