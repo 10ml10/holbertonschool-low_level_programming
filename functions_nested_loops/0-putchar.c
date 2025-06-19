6c71788db37968c1fcda49dd73e468968167a9c3
10ml10 <lisacarino10@gmail>
2025-06-19 09:01:35 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Point d'entrée du programme
+ *
+ * Return: Always 0 (Succès)
+ */
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
+}
