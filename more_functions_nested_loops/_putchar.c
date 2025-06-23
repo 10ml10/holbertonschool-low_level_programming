357e6e750d992b56c624195ae69cbd534ecd6fe1
10ml10 <lisacarino10@gmail.com>
2025-06-23 12:08:47 +0000

    3-print_numbers.c

---
+++ more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Écrit le caractère c sur stdout
+ * @c: Le caractère à imprimer
+ *
+ * Return: 1 si succès, -1 si erreur
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
